791e96a4be74fb3362f71d6c8b7726d3c98d4bff
Ksusha <Ksusha.matusha@gmail.com>
2023-05-24 16:03:01 +0300

    Ex5

---
+++ Ex5ParsingJSON.py
@@ -0,0 +1,10 @@
+import json
+
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+pars_json = json.loads(json_text)
+
+second_message = pars_json['messages'][1]
+print(f" это сообщение из задания:  {second_message['message']}")
+
+for i in pars_json['messages']:
+    print(f"это парсинг двух сообщений {i['message']}")
