ee2c3bcf039c9086ab1640f7bdd1cb787b6064d5
Ksusha <Ksusha.matusha@gmail.com>
2023-06-02 00:10:32 +0300

    Ex11

---
+++ ex/Ex11PrintCookie.py
@@ -0,0 +1,8 @@
+import pytest
+import requests
+
+
+def test_print_cookie():
+    response = requests.post("https://playground.learnqa.ru/api/homework_cookie")
+    print(response.cookies)
+    assert response.cookies["HomeWork"] == 'hw_value', f"Cookies != HomeWork=hw_value"
