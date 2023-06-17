cc2b28a42ca7be8e7c15e01c6f6aa5bc7c73d25d
Ksusha <Ksusha.matusha@gmail.com>
2023-06-17 22:41:25 +0300

    ex16

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -2,6 +2,7 @@ from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
+
 class TestUserGet(BaseCase):
     def test_get_user_details_not_auth(self):
         response = MyRequests.get("/user/2")
@@ -25,9 +26,36 @@ class TestUserGet(BaseCase):
         user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
         response2 = MyRequests.get(f"/user/{user_id_from_auth_method}",
-            headers = {"x-csrf-token": token},
-            cookies = {"auth_sid": auth_sid}
-        )
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid}
+                                   )
 
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_another_user(self):
+        data = self.prepare_registration_data()
+        response = MyRequests.post("/user", data=data)
+
+        Assertions.assert_code_status(response, 200)
+
+        self.data = {
+            "email": data['email'],
+            "password": data['password']
+        }
+
+        response1 = MyRequests.post("/user/login", data=self.data)
+        print(response1.content)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        response2 = MyRequests.get(f"/user/2",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid}
+                                   )
+
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
